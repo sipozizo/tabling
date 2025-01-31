81d90758ba646ed79337f8062a43ec3d676c67c7
박병천 <genius178896@gmail.com>
2025-01-31 20:22:12 +0900

    :sparkles Security Custom 기능 추가

--- src/main/java/sipozizo/tabling/security/CustomUserDetailService.java
+++ src/main/java/sipozizo/tabling/security/CustomUserDetailService.java
@@ -1,4 +1,29 @@
 package sipozizo.tabling.security;
 
-public class CustomUserDetailService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import sipozizo.tabling.user.entity.User;
+import sipozizo.tabling.user.repository.UserRepository;
+
+import java.util.NoSuchElementException;
+
+@RequiredArgsConstructor
+public class CustomUserDetailService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByName(username).orElseThrow();
+        return new CustomUserDetails(user);
+    }
+
+    public UserDetails loadUserByUserId(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new NoSuchElementException("유저가 존재하지 않습니다."));
+
+        return new CustomUserDetails(user);
+    }
 }
