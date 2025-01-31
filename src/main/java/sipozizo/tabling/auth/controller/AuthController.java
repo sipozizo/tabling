edf5fe2840a4c8738ed1be88b38cd078c660616c
박병천 <genius178896@gmail.com>
2025-01-31 20:18:34 +0900

    :sparkles User 회원가입 기능 추가

--- src/main/java/sipozizo/tabling/auth/controller/AuthController.java
+++ src/main/java/sipozizo/tabling/auth/controller/AuthController.java
@@ -1,4 +1,23 @@
 package sipozizo.tabling.auth.controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sipozizo.tabling.auth.dto.request.UserRegisterRequest;
+import sipozizo.tabling.auth.service.AuthService;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public void registerUser(@RequestBody UserRegisterRequest request) {
+        authService.registerUser(request);
+    }
 }
--- src/main/java/sipozizo/tabling/auth/service/AuthService.java
+++ src/main/java/sipozizo/tabling/auth/service/AuthService.java
@@ -1,4 +1,29 @@
 package sipozizo.tabling.auth.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.transaction.annotation.Transactional;
+import sipozizo.tabling.auth.dto.request.UserRegisterRequest;
+import sipozizo.tabling.user.entity.User;
+import sipozizo.tabling.user.repository.UserRepository;
+
+@RequiredArgsConstructor
 public class AuthService {
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional
+    public void registerUser(UserRegisterRequest request) {
+        Boolean isExist = userRepository.findByEmail(request.getEmail());
+
+        if (isExist) {
+            throw new IllegalStateException("이미 가입된 유저입니다.");
+        }
+
+        String encodePassword = passwordEncoder.encode(request.getPassword());
+        User user = User.registerUser(request, encodePassword);
+
+        userRepository.save(user);
+    }
 }
