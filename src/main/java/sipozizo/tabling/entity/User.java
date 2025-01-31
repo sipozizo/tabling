1219b033d25716a4c75f17fe06fcbc576f46e506
1000E <83916614+bottle1000@users.noreply.github.com>
2025-01-31 16:50:30 +0900

    Update User.java 최종 리뷰 후 확정

--- src/main/java/sipozizo/tabling/entity/User.java
+++ src/main/java/sipozizo/tabling/entity/User.java
@@ -2,9 +2,6 @@ package sipozizo.tabling.entity;
 
 
 import jakarta.persistence.*;
-// import jakarta.validation.constraints.Email;
-// import jakarta.validation.constraints.NotBlank;
-// import jakarta.validation.constraints.NotNull;
 
 @Entity
 @Table(name = "Users")
@@ -17,7 +14,6 @@ public class User extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    // @NotBlank(message = "이름은 공백이어서 안 됩니다.") 검증 애노테이션은 DTO에서 적용
     @Column(name = "name")
     private String name;
 
@@ -28,48 +24,19 @@ public class User extends BaseEntity {
     private String address;
 
     @NotBlank(message = "이메일은 공백이어서는 안 됩니다.")
-    // @Email(message = "유효한 이메일 주소여야 합니다.") 검증 애노테이션은 DTO에서 적용
     @Column(name = "email", unique = true)
     private String email;
 
-    // @NotBlank(message = "비밀번호는 공백이어서는 안됩니다.") 검증 애노테이션은 DTO에서 적용
     @Column(name = "password")
     private String password;
 
     @Enumerated(EnumType.STRING)
-    // @NotNull(message = "유저 역할은 null일 수 없습니다.") 검증 애노테이션은 DTO에서 적용
     @Column(name = "user_role")
     private UserRole userRole;
 
     @Column(name = "is_deleted")
     private boolean isDeleted;
 
-    // Update methods
-
-    public void updateName(String name) {
-        this.name = name;
-    }
-
-    public void updatePhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public void updateAddress(String address) {
-        this.address = address;
-    }
-
-    public void updateEmail(String email) {
-        this.email = email;
-    }
-
-    public void updatePassword(String password) {
-        this.password = password;
-    }
-
-    public void updateUserRole(UserRole userRole) {
-        this.userRole = userRole;
-    }
-------------------------------------------------------------------
     // 필요 생성자 추가
     public User(String name, String phoneNumber, String address, String email, String password, UserRole userRole) {
         this.name = name;
@@ -121,8 +88,6 @@ public class User extends BaseEntity {
             this.userRole = userRole;
         }
     }
-
-------------------------------------------------------------------
 
     public void deleteUser() {
         this.isDeleted = true;
