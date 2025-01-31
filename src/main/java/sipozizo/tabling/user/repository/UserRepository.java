ae05570a32506d53f15f13c615c33163a57f0fee
박병천 <genius178896@gmail.com>
2025-01-31 20:58:11 +0900

    :sparkles 쿼리 메서드 추가

--- src/main/java/sipozizo/tabling/user/repository/UserRepository.java
+++ src/main/java/sipozizo/tabling/user/repository/UserRepository.java
@@ -3,5 +3,11 @@ package sipozizo.tabling.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import sipozizo.tabling.user.entity.User;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    Boolean existsByEmail(String email);
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findByName(String name);
 }
