ce7ee4cb09a6c5ad9584ed84676299418f80c05a
박병천 <genius178896@gmail.com>
2025-01-31 20:57:07 +0900

    :sparkles 로그인 요청 DTO 추가

--- src/main/java/sipozizo/tabling/auth/dto/request/UserLoginRequest.java
+++ src/main/java/sipozizo/tabling/auth/dto/request/UserLoginRequest.java
@@ -1,4 +1,16 @@
 package sipozizo.tabling.auth.dto.request;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public class UserLoginRequest {
+
+    @Email
+    private String email;
+    @NotBlank
+    private String password;
 }
