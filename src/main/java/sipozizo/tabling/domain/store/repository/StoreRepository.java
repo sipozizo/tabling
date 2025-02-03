c8319e3cb49191edb57a1083dda7b843fe5b68fc
Kyeongran <eggking2580@gmail.com>
2025-02-03 11:40:47 +0900

    :art:chore: Modify build.gradle code structure

--- src/main/java/sipozizo/tabling/domain/store/repository/StoreRepository.java
+++ src/main/java/sipozizo/tabling/domain/store/repository/StoreRepository.java
@@ -1,7 +1,7 @@
 package sipozizo.tabling.domain.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import sipozizo.tabling.entity.Store;
+import sipozizo.tabling.common.entity.Store;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
 }
