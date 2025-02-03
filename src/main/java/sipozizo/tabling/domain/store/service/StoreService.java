9ec8e84e0a9a1bef504409ab081f9baacfbcae6d
Kyeongran <eggking2580@gmail.com>
2025-02-03 14:31:22 +0900

    commit for pull

--- src/main/java/sipozizo/tabling/domain/store/service/StoreService.java
+++ src/main/java/sipozizo/tabling/domain/store/service/StoreService.java
@@ -2,15 +2,24 @@ package sipozizo.tabling.domain.store.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Service;
+import sipozizo.tabling.common.entity.User;
+import sipozizo.tabling.domain.store.model.request.StoreRequest;
+import sipozizo.tabling.domain.store.model.response.StoreResponse;
 import sipozizo.tabling.domain.store.repository.StoreRepository;
+import sipozizo.tabling.security.CustomUserDetails;
 
 @Slf4j
 @Service
 @AllArgsConstructor
 public class StoreService {
-    private StoreRepository storeRepository;
-    //private UserRepository userRepository;
+    private final CacheManager cacheManager;
+    private final StoreRepository storeRepository;
+    // private final UserRepository userRepository;
 
-    
+    public StoreResponse createStore(@AuthenticationPrincipal CustomUserDetails user, StoreRequest request) {
+    }
 }
