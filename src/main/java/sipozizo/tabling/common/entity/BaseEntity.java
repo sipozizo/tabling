b97378628284944d3a01eb2aa22028ed9fbf8740
Kyeongran <eggking2580@gmail.com>
2025-02-04 09:53:07 +0900

    commit for pull

--- src/main/java/sipozizo/tabling/common/entity/BaseEntity.java
+++ src/main/java/sipozizo/tabling/common/entity/BaseEntity.java
@@ -5,7 +5,6 @@ import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
-
 import java.time.LocalDateTime;
 
 @Getter
--- src/main/java/sipozizo/tabling/domain/store/service/StoreService.java
+++ src/main/java/sipozizo/tabling/domain/store/service/StoreService.java
@@ -10,6 +10,9 @@ import sipozizo.tabling.domain.store.model.request.StoreRequest;
 import sipozizo.tabling.domain.store.repository.StoreRepository;
 import sipozizo.tabling.domain.user.repository.UserRepository;
 
+import java.sql.Time;
+import java.util.Optional;
+
 @Slf4j
 @Service
 @AllArgsConstructor
@@ -22,11 +25,11 @@ public class StoreService {
     public void createStore(StoreRequest request) {
 
         if (storeRepository.existsByStoreName(request.storeName())) {
-            throw new IllegalArgumentException("이미 존재하는 가게입니다.");
+            throw new IllegalArgumentException("이미 존재하는 가게입니다."); //todo 예외처리 변경 예정
         }
 
         if (storeRepository.existsByRegistrationNumber(request.registrationNumber())) {
-            throw new IllegalArgumentException("이미 존재하는 사업자등록번호입니다.");
+            throw new IllegalArgumentException("이미 존재하는 사업자등록번호입니다."); // todo 예외처리 변경 예정
         }
 
         Store store = Store.builder()
@@ -34,8 +37,8 @@ public class StoreService {
                 .storeNumber(request.storeNumber())
                 .storeAddress(request.storeAddress())
                 .registrationNumber(request.registrationNumber())
-                .openingTime(request.openingTime().toLocalTime())
-                .closingTime(request.closingTime().toLocalTime())
+                .openingTime(Optional.ofNullable(request.openingTime()).map(Time::toLocalTime).orElse(null))
+                .closingTime(Optional.ofNullable(request.closingTime()).map(Time::toLocalTime).orElse(null))
                 .category(request.category())
                 .build();
 
