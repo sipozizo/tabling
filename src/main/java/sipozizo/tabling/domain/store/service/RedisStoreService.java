51328d81065b85bad2da9b6a2146d5e1794d55ca
박병천 <genius178896@gmail.com>
2025-02-04 19:08:07 +0900

    :sparkles: 상점 조회와 동시에 조회수 상승하는 로직 V1

---
+++ src/main/java/sipozizo/tabling/domain/store/service/RedisStoreService.java
@@ -0,0 +1,34 @@
+package sipozizo.tabling.domain.store.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import sipozizo.tabling.common.entity.Store;
+import sipozizo.tabling.domain.store.model.response.StoreWithViewCountResponseV1;
+import sipozizo.tabling.domain.store.model.response.StoreWithViewCountResponseV2;
+import sipozizo.tabling.domain.store.repository.StoreRepository;
+
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.concurrent.TimeUnit;
+
+@Service
+@RequiredArgsConstructor
+public class RedisStoreService {
+
+    private final StoreRepository storeRepository;
+    private final RedisTemplate<String, Integer> countRedisTemplate;
+    private final RedisTemplate<String, Store> storeRedisTemplate;
+
+    // 상점 조회(캐시 사용x)
+    @Transactional
+    public StoreWithViewCountResponseV1 getStoreV1(Long storeId) {
+        Store store = storeRepository.findStoreById(storeId)
+                .orElseThrow(() -> new IllegalStateException("찾는 상점이 없습니다."));
+        store.incrementViewCount();
+        return StoreWithViewCountResponseV1.of(store);
+    }
+
+}
