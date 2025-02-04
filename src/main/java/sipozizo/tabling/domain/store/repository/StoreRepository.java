7b07dc3657c28f28521510e0bc80e4d6a140ce31
박병천 <genius178896@gmail.com>
2025-02-04 19:06:58 +0900

    :sparkles: id로 상점 조회를 하고 Optional로 반환 받는 기능 추가

--- src/main/java/sipozizo/tabling/domain/store/repository/StoreRepository.java
+++ src/main/java/sipozizo/tabling/domain/store/repository/StoreRepository.java
@@ -2,9 +2,12 @@ package sipozizo.tabling.domain.store.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import sipozizo.tabling.common.entity.Store;
+import java.util.Optional;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
     boolean existsByStoreName(String storeName);
 
     boolean existsByRegistrationNumber(String registrationNumber);
+
+    Optional<Store> findStoreById(Long id);
 }
