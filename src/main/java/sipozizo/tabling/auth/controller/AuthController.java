ae4c00e80720657766d27fcf0ed5b605ab4215f8
Kyeongran <eggking2580@gmail.com>
2025-02-07 10:09:38 +0900

    :broom:chore: Delete imports not in use

--- src/main/java/sipozizo/tabling/auth/controller/AuthController.java
+++ src/main/java/sipozizo/tabling/auth/controller/AuthController.java
@@ -5,7 +5,6 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sipozizo.tabling.auth.dto.request.UpdateUserRoleRequest;
